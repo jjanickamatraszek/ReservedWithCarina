0299d775012379a3b409a411ad84356ee9cc63ac
jjanickamatraszek <jjanickamatraszek@solvd.com>
2023-05-28 14:14:26 +0200

    add test for switching context between webview and chrome native app
    for context switch there is MobileContextUtils class used with code decribed in Carina doc

---
+++ src/main/java/com/solvd/android/nativeChromePages/ChromeMainPage.java
@@ -0,0 +1,21 @@
+package com.solvd.android.nativeChromePages;
+
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
+import com.qaprosoft.carina.core.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class ChromeMainPage extends AbstractPage {
+
+    @FindBy(id = "com.android.chrome:id/home_button")
+    ExtendedWebElement homeBtn;
+
+    public ChromeMainPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public GoogleHomePage goToHomePage(){
+        homeBtn.click();
+        return new GoogleHomePage(driver);
+    }
+}
---
+++ src/test/java/com/solvd/tests/ContextTest.java
@@ -0,0 +1,37 @@
+package com.solvd.tests;
+
+import com.qaprosoft.carina.core.foundation.IAbstractTest;
+import com.solvd.android.nativeChromePages.ChromeMainPage;
+import com.solvd.android.nativeChromePages.GoogleHomePage;
+import com.solvd.common.pages.HomePageBase;
+import com.solvd.utils.MobileContextUtils;
+import com.zebrunner.carina.utils.Configuration;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ContextTest implements IAbstractTest {
+
+    @Test
+    public void switchContextTest() {
+        HomePageBase reservedHomePage = initPage(getDriver(), HomePageBase.class);
+        reservedHomePage.goToPage();
+
+        MobileContextUtils contextHelper = new MobileContextUtils();
+        contextHelper.switchMobileContext(MobileContextUtils.View.NATIVE);
+
+        GoogleHomePage googleHomePage = (new ChromeMainPage(getDriver())).goToHomePage();
+
+        Assert.assertTrue(googleHomePage.isGoogleLogoDisplayed());
+
+        googleHomePage.searchFirstEntryForPhrase(Configuration.get(Configuration.Parameter.URL));
+
+        contextHelper.switchMobileContext(MobileContextUtils.View.WEB_VIEW);
+
+        initPage(getDriver(), HomePageBase.class)
+                .getCookieDialog()
+                .acceptCookies();
+        reservedHomePage = initPage(getDriver(), HomePageBase.class);
+
+        Assert.assertTrue(reservedHomePage.isLogoVisible());
+    }
+}
